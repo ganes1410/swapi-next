9e7cbe03f2717d9afe1bf8d40ecefe4516494152
Ganesh R <ganesramesh110@gmail.com>
2022-02-19 15:38:19 +0530

    feat: setup chakra ui for styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app";
 import { Provider } from "urql";
 import { apiClient } from "../lib/api/apiClient";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider value={apiClient}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </Provider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useFilmsListQuery } from "../lib/generated/graphql";
+import { Box } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   const [result] = useFilmsListQuery();
@@ -9,18 +10,21 @@ const Home: NextPage = () => {
   if (result.error) return <p>Error</p>;
 
   return (
-    <div>
+    <Box>
       <Head>
         <title>Swapi</title>
         <meta name="description" content="An App built with swapi api" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {result.data?.allFilms?.edges?.map((film) => (
-        <div key={film?.node?.id}>
-          <p>{film?.node?.openingCrawl}</p>
-        </div>
-      ))}
-    </div>
+
+      <Box>
+        {result.data?.allFilms?.edges?.map((film) => (
+          <Box key={film?.node?.id} mt="2">
+            <p>{film?.node?.openingCrawl}</p>
+          </Box>
+        ))}
+      </Box>
+    </Box>
   );
 };
 
